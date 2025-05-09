266871741809921853aca53aa312cde24d7e2e6a
rejo132 <enoch.luke1@student.moringaschool.com>
2025-05-09 10:39:29 +0300

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fibonacci = []
+    if length >= 1:
+        fibonacci.append(0)
+    if length >= 2:
+        fibonacci.append(1)
+    for i in range(2, length):
+        fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+    
+    print(fibonacci)
+    
